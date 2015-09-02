d127efeb8ab4c07f317bc45588d27e761889e2f2
Paramount2009 <Paramount7891@gmail.com>
2015-09-02 12:46:10 +0300

    modified style

--- js/index.js
+++ js/index.js
@@ -1,6 +1,52 @@
 $(document).ready(function(){
 
-	////
+
+var videoInput = '<div class="video-item"><input type="text" class="form-control"><span class="close">х</span></div>';
+var contactInput = '<div class="form-contact-item form-contact-item-added"><div class="close">удалить контакт</div><div class="form-group"><label>Телефон</label><input type="tel" class="form-control"></div><div class="form-group"><label>Электронная почта</label><input type="email" class="form-control"></div></div>'
+var addressInput = '<div class="form-address-item form-address-item-added"><div class="close">удалить адрес</div><div class="form-group"><label class="label-strict">Улица</label><input type="text" class="form-control form-control-strict" name="streetPage" required></div><div class="form-group-wrap"><div class="form-group form-group-inline pull-left"><label class="label-strict">Дом</label><input type="text" class="form-control form-control-strict" name="housePage" required></div><div class="form-group form-group-inline pull-right"><label>Квартира</label><input type="text" class="form-control"></div></div><div class="form-group"><label>Хотите что-то добавить?</label><input type="text" class="form-control"></div></div>';
+/////////////////////////////video add
+$("#video-plus").click(function(e){
+    e.preventDefault();
+    $(".form-video .form-group").append(videoInput);
+    $(".form-video .video-item").addClass("aviable-close");
+});
+
+$(".form-video").on("click", ".video-item .close",function(){
+  $(this).closest(".video-item").remove();
+  if($(".video-item").length < 2){
+    $(".video-item").removeClass("aviable-close");
+  }
+  
+});
+////////////////////////////////////contact add
+$("#contact-plus").click(function(e){
+    e.preventDefault();
+    $(".form-contact-inner").append(contactInput);
+});
+
+$(".form-contact").on("click", ".form-contact-item-added .close",function(){
+  $(this).closest(".form-contact-item-added").remove();
+});
+////////////////////////////////////////////address add
+$("#address-plus").click(function(e){
+    e.preventDefault();
+    $(".form-address-inner").append(addressInput);
+
+});
+
+$(".form-address").on("click", ".form-address-item-added .close",function(){
+  $(this).closest(".form-address-item-added").remove();
+});
+
+//////////////////////////////////////////trigger-event
+$("#trigger-event").change(function(){
+  if($(this).is(":checked")){
+    $(".form-address").slideUp();
+  }else{
+    $(".form-address").slideDown();
+  }
+});
+//////////////////////////////////trigger history
 	$(".full-link").click(function(){
 
 	  var target = 	$(this).prev(".short-outer").toggleClass("open");
@@ -31,10 +77,55 @@ $('.color-carousel').owlCarousel({
         }
     }
 });
-////////
+
+$('.color-carousel').on("click",".color",function(){
+
+  if($(this).hasClass("active")){
+    return;
+  }else{
+    $('.color-carousel .color').removeClass("active");
+    $(this).addClass('active');
+  }
+
+});
+//////////////////////////////////////form-page
+
+var validatorForm =  $("#form-page").validate({
+      rules: {
+        descPage: "required",
+        moneyPage: "required",
+        deadlinePage: "required",
+        urlPage: "required",
+        cityPage: "required",
+        datePage: "required",
+        timePage: "required",
+        titlePage: {
+          required: true,
+          minlength: 3
+        }
+      },
+      messages: {
+        descPage: "Напишите краткое описание",
+        moneyPage: false,
+        deadlinePage: false,
+        urlPage: false,
+        cityPage: false,
+        datePage: false,
+        timePage: false,
+        streetPage: false,
+        housePage: false,
+        titlePage: {
+          required: "Заполните заголовок",
+          minlength: "Заголовок должен быть не менее 3 символов"
+        }
+      }
+    });
 
 
 
+$("#deadline-page").mask("99.99.9999",{placeholder:"дд/мм/год "});
+$("#date-page").mask("99.99.9999",{placeholder:"дд/мм/год "});
+$("#time-page").mask("99:99",{placeholder:"__:__ "});
 
 	////////////////////////////////map
           var myMap;
